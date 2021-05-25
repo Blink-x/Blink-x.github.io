9922882a8f7eb2c0577c002de00d028dae848b40
Blink-x <BrianLink89@gmail.com>
2021-05-24 22:07:23 -0700

    fixed minutes to have 0 when necessary

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ let hours = fullDate.getHours();
 let minutes = fullDate.getMinutes();
 let secs = fullDate.getSeconds();
 
-if (minutes < 10) {mintues = "0" + minutes};
+if (minutes < 10) {minutes = "0" + minutes};
 if (secs < 10) {secs = "0" + secs};
 if (hours > 12) {hours = hours -12};
 if (hours > 12) {hours = "0"+ (hours -12)}

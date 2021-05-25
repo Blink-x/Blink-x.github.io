d3ade53c895b6ede3fe6c69541669f4d990e6758
Blink-x <BrianLink89@gmail.com>
2021-05-24 22:00:58 -0700

    Added playlist to bottom

--- scripts.js
+++ scripts.js
@@ -5,13 +5,11 @@ let hours = fullDate.getHours();
 let minutes = fullDate.getMinutes();
 let secs = fullDate.getSeconds();
 
-
 if (minutes < 10) {mintues = "0" + minutes};
 if (secs < 10) {secs = "0" + secs};
 if (hours > 12) {hours = hours -12};
 if (hours > 12) {hours = "0"+ (hours -12)}
 else if (hours < 10) {hours = "0" + hours};
-
 
 document.getElementById('hours').innerHTML = hours;
 document.getElementById('minutes').innerHTML = minutes;
